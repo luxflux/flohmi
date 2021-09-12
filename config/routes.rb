db3d389708b3a68cfefdd1a8f0a3ca9d39d1fddd
Raffael Schmid <raffael@yux.ch>
2021-09-12 10:54:58 +0200

    Add login

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   resources :articles
 
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+
   root "articles#index"
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_12_083157) do
+ActiveRecord::Schema.define(version: 2021_09_12_084528) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -44,6 +44,13 @@ ActiveRecord::Schema.define(version: 2021_09_12_083157) do
     t.string "title", null: false
     t.text "description", null: false
     t.decimal "price", precision: 19, scale: 4, null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email"
+    t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
