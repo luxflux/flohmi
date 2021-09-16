0931bed9b4d367be12048e6d4ad3d01136519cb0
Raffael Schmid <raffael@yux.ch>
2021-09-16 21:22:24 +0200

    null: false pls

--- db/schema.rb
+++ db/schema.rb
@@ -49,8 +49,8 @@ ActiveRecord::Schema.define(version: 2021_09_12_084528) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "email"
-    t.string "password_digest"
+    t.string "email", null: false
+    t.string "password_digest", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
