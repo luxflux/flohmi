228de9e2c04820d27ce811042afd0c3a7641c555
Raffael Schmid <raffael@yux.ch>
2021-09-18 22:11:05 +0200

    Add Postmark config for production

--- app/mailers/contact_request_mailer.rb
+++ app/mailers/contact_request_mailer.rb
@@ -1,6 +1,6 @@
 class ContactRequestMailer < ApplicationMailer
   def created(contact_request)
     @contact_request = contact_request
-    mail to: ENV.fetch('MAIL_RECEIVER'), subject: 'Neue Flohmi Anfrage'
+    mail to: ENV.fetch('MAIL_RECEIVER'), from: 'flohmi@yux.ch', subject: 'Neue Flohmi Anfrage'
   end
 end
